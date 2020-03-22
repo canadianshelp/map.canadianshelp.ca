1aa1ce7c5903f401f1c9a9fdb501b78e43b92ea8
Daniel Paz-Soldan <daniel.pazsoldan@gmail.com>
2020-03-22 17:15:53 -0400

    Updates

---
+++ precache-manifest.844bf4919bc3b803b0601b57280081af.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3e4089e667cd089e26eceb68e9195b8f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d95a91e1f1e29024cbbe",
+    "url": "/static/css/2.89e250a1.chunk.css"
+  },
+  {
+    "revision": "3c053dfb98a50a00acf1",
+    "url": "/static/css/main.97f8f30f.chunk.css"
+  },
+  {
+    "revision": "d95a91e1f1e29024cbbe",
+    "url": "/static/js/2.30e2b1f8.chunk.js"
+  },
+  {
+    "revision": "a11a159c1d1bcc971557124ac13cc921",
+    "url": "/static/js/2.30e2b1f8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3c053dfb98a50a00acf1",
+    "url": "/static/js/main.3fbd8e25.chunk.js"
+  },
+  {
+    "revision": "54d575e054a9491a4a46",
+    "url": "/static/js/runtime-main.9e3c8587.js"
+  }
+]);
